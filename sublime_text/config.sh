1467d7db9714c82fdd8bb8f5a2290a3b9d3b5015
B. R. Mullikin <ben@brmullikin.com>
2016-11-04 15:14:27 -0500

    correct sublime text shell script.

--- sublime_text/config.sh
+++ sublime_text/config.sh
@@ -7,7 +7,6 @@
 # 3. Install bundled Package Control
 # 4. Symlink packages
 # 5. Symlink preferances
-# 6. Symlink Subl
 
 # symlink subl
 # ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" "/usr/local/bin/subl"
@@ -17,6 +16,7 @@
 if test $(which brew); then
 
     # Install Sublime text via the command line
+    # This also installs subl to your path
     brew cask install sublime-text
     echo "Sublime Text installed"
 
@@ -24,17 +24,14 @@ if test $(which brew); then
     brew cask install font-hack
     echo "Font Hack Installed"
 
-    # Symobolic link for sublime text CLI launch.
-    ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" "/usr/local/bin/subl"
-
     # Install Package Controll
-    cp "sublime_files/Package Control.sublime-package" "~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package Control.sublime-package"
+    cp $PWD/sublime_text/sublime_files/Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package
 
     # Symbolic link for sublime text packages
-    mv "~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package Control.sublime-settings" "~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package Control.sublime-settings.old"
-    ln -s "sublime_files/Package Control.sublime-settings" "~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package Control.sublime-settings"
+    mv ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings.old
+    ln -s $PWD/sublime_text/sublime_files/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
 
     # Symbolic link for sublime text packages
-    mv "~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Preferences.sublime-settings" "~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Preferences.sublime-settings.old"
-    ln -s "sublime_files/Preferences.sublime-settings" "~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Preferences.sublime-settings"
+    mv ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings.old
+    ln -s $PWD/sublime_text/sublime_files/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 fi
